472bf16f01b50a34b9cff1b35bb95b0830adcf51
roland <rsx@bluewin.ch>
2018-03-02 17:52:50 +0100

    Fix broken example

--- src/AnaTreeSorted.hs
+++ src/AnaTreeSorted.hs
@@ -13,5 +13,5 @@ anaTreeSorted = ana coalg where
     coalg (x : xs) = NodeF (filter (<= x) xs) x (filter (> x) xs)
 
 -- $
--- >>> anaTreeSorted [1,6,10,10,9,4,3, 5]
--- Node (Node (Node Leaf 3 Leaf) 2 (Node Leaf 4 Leaf)) 1 (Node (Node Leaf 6 Leaf) 5 (Node Leaf 7 Leaf))
+-- >>> anaTreeSorted [6,10,10,9,4,5,1,3]
+-- Node (Node (Node Leaf 1 (Node Leaf 3 Leaf)) 4 (Node Leaf 5 Leaf)) 6 (Node (Node (Node Leaf 9 Leaf) 10 Leaf) 10 Leaf)
