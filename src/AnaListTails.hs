5a8ab89659fa915fc6c6e2a42e339bec441a1363
roland <rsx@bluewin.ch>
2018-03-26 10:59:13 +0200

    Improve doctest tests

--- src/AnaListTails.hs
+++ src/AnaListTails.hs
@@ -1,9 +1,12 @@
 module AnaListTails where 
 
 import Data.Functor.Foldable
-    
+import Data.List (tails)                      -- just for our test
+ 
 -- | All tails of a list using an anamorphism
 --   Note, this does not add the empty list [] to the result
+--   So it's NOT equivalent to the tails function of Data.List
+--   See src/ApoListTails.hs
 anatails :: [a] -> [[a]]
 anatails = ana coalg where
     coalg :: [a] -> ListF [a] [a]
@@ -15,3 +18,7 @@ anatails = ana coalg where
 -- [[1,2,3,4],[2,3,4],[3,4],[4]]
 -- >>> anatails []
 -- []
+-- >>> anatails [1,2,3,4] == (tails [1,2,3,4])
+-- False
+-- >>> anatails [] == (tails [])
+-- False
--- src/ApoListTails.hs
+++ src/ApoListTails.hs
@@ -1,9 +1,11 @@
 module ApoListTails where 
 
 import Data.Functor.Foldable
-        
+import Data.List (tails)                      -- just for our test
+       
 -- | All tails of a list using an apomorphism
 --   Compare this with the file AnaListTails.hs 
+--   This is equivalent to the tails function of Data.List
 apotails :: [Int] -> [[Int]]
 apotails = apo coalg where
     coalg :: [Int] -> ListF [Int] (Either [[Int]] [Int])
@@ -15,3 +17,7 @@ apotails = apo coalg where
 -- [[1,2,3,4],[2,3,4],[3,4],[4],[]]
 -- >>> apotails []
 -- [[]]
+-- >>> apotails [1,2,3,4] == (tails [1,2,3,4])
+-- True
+-- >>> apotails [] == (tails [])
+-- True
