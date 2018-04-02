3f8388fa7d0f42067669a9f0397a278fd1e0620f
roland <rsx@bluewin.ch>
2018-04-02 17:36:48 +0200

    Replace ParaListDedup by CataListDedup

--- src/CataListDedup.hs
+++ src/CataListDedup.hs
@@ -1,14 +1,14 @@
 {-# LANGUAGE ScopedTypeVariables #-}
-module ParaListDedup where 
+module CataListDedup where 
 
 import Data.Functor.Foldable
 
 -- | Remove duplicates from a list 
 dedup :: forall a.Eq a => [a] -> [a]
-dedup = para alg where 
-    alg :: ListF a ([a], [a]) -> [a]
+dedup = cata alg where 
+    alg :: ListF a [a] -> [a]
     alg Nil                 = []
-    alg (Cons x (inps, xs)) = if x `elem` inps then xs else x:xs
+    alg (Cons x xs) = if x `elem` xs then xs else x:xs
 
 -- $
 -- >>> dedup [1,2,3,4,5,6,5,4,3,2,1] 
